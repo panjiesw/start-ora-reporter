723782fe9a4062b9d0723a54563a8ca61db0bb60
Panjie Setiawan Wicaksono <panjie@panjiesw.com>
2018-09-29 17:18:20 +0700

    feat: ban plugins from starting spinner
    A naive implementation to exclude certain
    plugins from in spinners (like `parallel` or `xargs`.
    Also improve readability of task color
    by using colorwheel and task name hash

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,39 @@ type StartError = Error | string[] | string | null;
 
 const key = (task: string, plugin: string) => `${task}.${plugin}`;
 
+/**
+ * Hash code adapted from java and modified as needed.
+ *
+ * @param str String to calculate hash code
+ */
+const hash = (str: string) => {
+  let h = 0;
+  for (let i = 0; i < str.length; i++) {
+    const code = str.charCodeAt(i);
+    // tslint:disable-next-line:no-bitwise
+    h = (h << 5) - h + code;
+    // tslint:disable-next-line:no-bitwise
+    h = h & h;
+  }
+  return h;
+};
+
+const bannedStart = ['parallel', 'xargs'];
+const colors = ['cyan', 'green', 'blue', 'red', 'yellow', 'magenta'];
+const NUM_COLOR = colors.length;
+
 export default (task: string) => {
   const emitter = new EventEmitter();
+  const color = colors[Math.abs(hash(task)) % NUM_COLOR];
   const spinners: { [index: string]: any } = {};
   const fileCounts: { [index: string]: number } = {};
 
   emitter.on('start', (plugin: string) => {
     const k = key(task, plugin);
-    if (plugin !== 'watch') {
+    if (!bannedStart.includes(plugin)) {
       if (!spinners[k]) {
         spinners[k] = ora({
-          text: `Running ${chalk.cyan.bold(task)}.${chalk.magenta(plugin)}`,
+          text: `${chalk[color].bold(task)} ${chalk.white.bold(plugin)}`,
         });
       }
       spinners[k].start();
@@ -24,70 +46,71 @@ export default (task: string) => {
   });
 
   emitter.on('message', (plugin: string, message: string) => {
-    if (plugin !== 'watch') {
-      spinners[key(task, plugin)].text = `Running ${chalk.cyan.bold(
+    if (!bannedStart.includes(plugin)) {
+      spinners[key(task, plugin)].text = `${chalk[color].bold(
         task,
-      )}.${chalk.magenta(plugin)}: ${chalk.yellow(message)}`;
-    } else {
-      // tslint:disable-next-line:no-console
-      console.log(
-        `${chalk.cyan.bold(task)}.${chalk.magenta(plugin)}: ${chalk.yellow(
-          message,
-        )}`,
-      );
+      )} ${chalk.white.bold(plugin)}: ${chalk.yellow(message)}`;
     }
   });
 
   emitter.on('file', (plugin: string) => {
-    const k = key(task, plugin);
-    if (!fileCounts[k]) {
-      fileCounts[k] = 0;
-    }
-    fileCounts[k] += 1;
+    if (!bannedStart.includes(plugin)) {
+      const k = key(task, plugin);
+      if (!fileCounts[k]) {
+        fileCounts[k] = 0;
+      }
+      fileCounts[k] += 1;
 
-    spinners[k].text = `Running ${chalk.cyan.bold(task)}.${chalk.magenta(
-      plugin,
-    )}: ${chalk.blue(`processed ${fileCounts[k].toString()}`)}`;
+      spinners[k].text = `${chalk[color].bold(task)} ${chalk.white.bold(
+        plugin,
+      )}: ${chalk.blue(`processed ${fileCounts[k]}`)}`;
+    }
   });
 
   emitter.on('done', (plugin: string) => {
-    const k = key(task, plugin);
-    const files = fileCounts[k];
-    let message = '';
-    if (files) {
-      message = `: ${chalk.green(`processed ${files.toString()} files`)}`;
-    }
+    if (!bannedStart.includes(plugin)) {
+      const k = key(task, plugin);
+      const files = fileCounts[k];
+      let message = '';
+      if (files != null) {
+        message = `: ${chalk.green(
+          `processed ${files} file${files > 1 ? 's' : ''}`,
+        )}`;
+      }
 
-    spinners[k].succeed(
-      `Finished ${chalk.cyan.bold(task)}.${chalk.magenta(plugin)}${message}`,
-    );
-    fileCounts[k] = 0;
+      spinners[k].succeed(
+        `${chalk[color].bold(task)} ${chalk.white.bold(plugin)}${message}`,
+      );
+      fileCounts[k] = 0;
+    }
   });
 
   emitter.on('error', (plugin: string, error: StartError) => {
-    const k = key(task, plugin);
-    if (error instanceof Error) {
-      spinners[k].fail(
-        `Error on ${chalk.cyan.bold(task)}.${chalk.magenta(
-          plugin,
-        )}: ${chalk.red(error.message)}`,
-      );
-    } else if (Array.isArray(error)) {
-      spinners[k].fail(
-        `Errors on ${chalk.cyan.bold(task)}.${chalk.magenta(
-          plugin,
-        )}: ${chalk.red(JSON.stringify(error))}`,
-      );
-    } else if (typeof error === 'string') {
-      spinners[k].fail(
-        `Error on ${chalk.cyan.bold(task)}.${chalk.magenta(
-          plugin,
-        )}: ${chalk.red(error)}`,
-      );
-    } else {
-      spinners[k].fail(
-        `Error on ${chalk.cyan.bold(task)}.${chalk.magenta(plugin)}`,
-      );
+    if (!bannedStart.includes(plugin)) {
+      const k = key(task, plugin);
+      if (error instanceof Error) {
+        spinners[k].fail(
+          `${chalk[color].bold(task)} ${chalk.white.bold(plugin)}: ${chalk.red(
+            error.message,
+          )}`,
+        );
+      } else if (Array.isArray(error)) {
+        spinners[k].fail(
+          `${chalk[color].bold(task)} ${chalk.white.bold(plugin)}: ${chalk.red(
+            JSON.stringify(error),
+          )}`,
+        );
+      } else if (typeof error === 'string') {
+        spinners[k].fail(
+          `${chalk[color].bold(task)} ${chalk.white.bold(plugin)}: ${chalk.red(
+            error,
+          )}`,
+        );
+      } else {
+        spinners[k].fail(
+          `${chalk[color].bold(task)} ${chalk.white.bold(plugin)}`,
+        );
+      }
     }
   });
 
